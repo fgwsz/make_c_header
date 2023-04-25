515c82d05c6ff9c7a5dbf6663c14acec2427a494
fgwsz <3191000458@qq.com>
2023-04-25 13:40:37 +0800

    change some header file

--- include/memory.h
+++ include/memory.h
@@ -1,12 +1,12 @@
 #ifndef HF_MEMORY_H
 #define HF_MEMORY_H
-#ifdef __cplusplus
-extern "C"{
-#endif
 #include<stddef.h> // size_t
+#ifdef __cplusplus
+    extern "C"{
+#endif
 extern void* memory_alloc(size_t size);
 extern void  memory_free(void* ptr);
 #ifdef __cplusplus
-}
+    }
 #endif
 #endif//!HF_MEMORY_H
--- include/pwd.h
+++ include/pwd.h
@@ -1,14 +1,11 @@
 #ifndef HF_PWD_H
 #define HF_PWD_H
-
+#include<stddef.h> // size_t
 #ifdef __cplusplus
     extern "C"{
 #endif
-#include<stddef.h> // size_t
 extern void pwd(char* buffer,size_t byte_size);
-
 #ifdef __cplusplus
     }
 #endif
-
 #endif
